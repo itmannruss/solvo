081ef8a8474f5082340a866eb74bf4c08973a26d
itmannruss@gmail.com <itmannruss@gmail.com>
2023-02-18 11:58:14 +0300

    2

--- src/App.js
+++ src/App.js
@@ -2,21 +2,17 @@ import Header from './components/Header/Header';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/Auth/Login';
 import Register from './pages/Auth/Register';
-import Home from './pages/Home/Home.jsx';
 import Detail from './pages/Detail/Detail';
-import AddPost from './pages/AddPost/AddPost';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <Header />
-        <AddPost />
+        <Header isAuth={false} />
+
         <Routes>
-          <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/detail" element={<Detail />} />
           <Route path="/detail" element={<Detail />} />
           <Route path="*" element="not found" />
         </Routes>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,24 +1,49 @@
 import './header.scss';
 import { Link } from 'react-router-dom';
 import Logo from '../../access/logo.svg';
+import Avatarka from '../../access/avatarka.png';
+import PointDonw from '../../access/point_donw.svg';
 import Button from '../Form/Button/Button';
 
-const Header = () => {
+const Header = ({ isAuth }) => {
   return (
-    <div className="header">
-      <div className="header_navbar container">
-        <Link to="/">
-          <img src={Logo} alt="" />
-        </Link>
+    <>
+      {isAuth ? (
+        <div className="header">
+          <div className="header_navbar container">
+            <Link to="/">
+              <img src={Logo} alt="" />
+            </Link>
 
-        <div className="header_navbar_btn">
-          <Button to="/login">Войти</Button>
-          <Button to="/register" className="btn-outline">
-            Регистрация
-          </Button>
+            <div className="header_navbar_right">
+              <Button to="/">Добавить пост</Button>
+              <div className="header_navbar_avatar">
+                <Link to="/">
+                  <img src={Avatarka} alt="" className="avatarka" />
+                </Link>
+
+                <img src={PointDonw} alt="" className="avatarka_pointDown" />
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      ) : (
+        <div className="header">
+          <div className="header_navbar container">
+            <Link to="/">
+              <img src={Logo} alt="" />
+            </Link>
+
+            <div className="header_navbar_btn">
+              <Button to="/login">Войти</Button>
+              <Button to="/register" className="btn-outline">
+                Регистрация
+              </Button>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
